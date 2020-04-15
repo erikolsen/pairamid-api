5e373143e3f619842717b92eaa611bbd69b85d02
Erik Olsen <erik.s.olsen@gmail.com>
2020-04-14 21:51:38 -0500

    Only show error to originating user

--- pairamid_api/pairing_session/events.py
+++ pairamid_api/pairing_session/events.py
@@ -5,17 +5,21 @@ from . import operations
 def create(json, methods=['GET', 'POST']):
     new_pair = operations.run_create()
     socketio.emit('add pair', new_pair)
+    return True
 
 @socketio.on('delete pair')
 def delete(json, methods=['GET', 'POST']):
     uuid = operations.run_delete(json['uuid'])
     socketio.emit('delete pair', uuid)
+    return  True
 
 @socketio.on('batch update pairs')
 def batch_update(json,methods=['GET', 'POST']):
     pairs = operations.run_batch_update(json)
     socketio.emit('batch update pairs', pairs)
+    return  True
 
 @socketio.on_error_default
 def default_error_handler(e):
-    socketio.emit('server error', {'message': str(e)})
+    print('\n\n {e} \n\n')
+    return {'error': True, 'message': str(e) }
