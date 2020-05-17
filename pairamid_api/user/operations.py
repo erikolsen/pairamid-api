906acc96dfb5896bf93f661156ac6f977eeb3db3
Erik Olsen <erik.s.olsen@gmail.com>
2020-05-16 21:45:52 -0500

    Add user crud

--- pairamid_api/user/operations.py
+++ pairamid_api/user/operations.py
@@ -1,12 +1,30 @@
-from pairamid_api.models import User, UserSchema
+from pairamid_api.models import User, UserSchema, Role
+from pairamid_api.extensions import db
 from sqlalchemy import asc, desc
 
 def run_fetch_all():
-    roles = User.query.order_by(asc(User.username)).all()
+    users = User.query.order_by(asc(User.username)).all()
     schema = UserSchema(many=True)
-    return schema.dump(roles) 
+    return schema.dump(users) 
 
 def run_update(id, data):
-    print('id', id)
-    print('data', data)
-    pass
+    user = User.query.get(id)
+    role = Role.query.get(data['roleId'])
+    user.role = role
+    user.username = data['initials'].upper()
+    db.session.add(user)
+    db.session.commit()
+    schema = UserSchema()
+    return schema.dump(user)
+
+def run_create(data):
+    user = User()
+    db.session.add(user)
+    db.session.commit()
+    schema = UserSchema()
+    return schema.dump(user)
+
+def run_delete(id):
+    User.query.filter(User.id == id).delete()
+    db.session.commit()
+    return id
--- pairamid_api/user/routes.py
+++ pairamid_api/user/routes.py
@@ -7,6 +7,14 @@ blueprint = Blueprint('user', __name__)
 def index():
     return jsonify(operations.run_fetch_all())
 
+@blueprint.route("/user", methods=["POST"])
+def create():
+    return jsonify(operations.run_create(request.json))
+
 @blueprint.route("/user/<id>", methods=["PUT"])
 def update(id):
-    return jsonify(operations.run_update(id, request.data))
+    return jsonify(operations.run_update(id, request.json))
+
+@blueprint.route("/user/<id>", methods=["DELETE"])
+def delete(id):
+    return jsonify(operations.run_delete(id))
