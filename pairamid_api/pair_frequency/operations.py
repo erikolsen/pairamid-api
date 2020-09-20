ac8d9a5523df4a155e7b99de3c485ca42cce36ad
Erik Olsen <erik.s.olsen@gmail.com>
2020-09-19 22:36:22 -0500

    Refactor frequencies

--- pairamid_api/pair_frequency/operations.py
+++ pairamid_api/pair_frequency/operations.py
@@ -1,28 +1,12 @@
 from pairamid_api.extensions import db
-from pairamid_api.models import User, PairingSession, Role, Team, Participants
-from sqlalchemy import asc, desc
-from sqlalchemy.orm import load_only
+from pairamid_api.models import User, PairingSession, Role, Team
+from collections import Counter
 
-def session_ids_for(user):
-    return [
-        ps.id
-        for ps in user.pairing_sessions.filter(
-            ~PairingSession.info.in_(PairingSession.FILTERED)
-        )
-    ]
-
-def number_of_times_paired(user1, user2):
-    if user1 is user2:
-        return len([p for p in user1.pairing_sessions.filter(
-            ~PairingSession.info.in_(PairingSession.FILTERED)
-        ) if len(p.users) == 1])
-
-    return [
-        p.user.username
-        for p in Participants.query.filter(
-            Participants.pairing_session_id.in_(session_ids_for(user1))
-        )
-    ].count(user2.username)
+def frequencies_for_user(user, group):
+    sessions = user.pairing_sessions.filter(~PairingSession.info.in_(PairingSession.FILTERED))
+    counts = Counter([u.username for pair in sessions for u in pair.users])
+    counts[user.username] -= sum(counts.values()) // 2
+    return [user.username, *[counts.get(u.username, 0) for u in group]]
 
 def run_build_frequency(team_uuid, primary, secondary):
     team = Team.query.filter(Team.uuid == team_uuid).first()
@@ -30,17 +14,8 @@ def run_build_frequency(team_uuid, primary, secondary):
     secondary = team.roles.filter(Role.name == secondary).first()
     primary_users = primary.users.all() if primary else team.users.all()
     secondary_users = secondary.users.all() if secondary else team.users.all()
-
-    history = []
-    for user1 in primary_users:
-        history.append(
-            [user1.username]
-            + [
-                number_of_times_paired(user1, user2)
-                for user2 in secondary_users
-            ]
-        )
+ 
     return {
-        "header": [" "] + [user.username for user in secondary_users],
-        "pairs": history,
+        "header": [" "] + [u.username for u in secondary_users],
+        "pairs": [frequencies_for_user(user, secondary_users) for user in primary_users],
     }
