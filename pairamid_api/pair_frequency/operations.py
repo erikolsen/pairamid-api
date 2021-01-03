8da5d1c0afba1bab285e0cc85922b24ad2db5bd4
Erik Olsen <erik.s.olsen@gmail.com>
2021-01-03 17:16:45 -0600

    Make frequency api take date range

--- pairamid_api/pair_frequency/operations.py
+++ pairamid_api/pair_frequency/operations.py
@@ -1,14 +1,25 @@
+from datetime import datetime 
+from pairamid_api.lib.date_helpers import start_of_day, end_of_day
 from pairamid_api.extensions import db
 from pairamid_api.models import User, PairingSession, Role, Team
 from collections import Counter
 
-def frequencies_for_user(user, group):
-    sessions = user.pairing_sessions.filter(~PairingSession.info.in_(PairingSession.FILTERED))
+def parse_date(iso_date_string):
+    return datetime.strptime(iso_date_string, '%Y-%m-%d')
+
+def frequencies_for_user(user, group, start, end):
+    start = start_of_day(parse_date(start))
+    end = end_of_day(parse_date(end))
+    sessions = (user.pairing_sessions
+                    .filter(~PairingSession.info.in_(PairingSession.FILTERED))
+                    .filter(PairingSession.created_at >= start)
+                    .filter(PairingSession.created_at <= end)
+    )
     counts = Counter([u.username for pair in sessions for u in pair.users if u is not user])
     counts[user.username] = len([p for p in sessions if len(p.users) == 1])
     return [user.username, *[counts.get(u.username, 0) for u in group]]
 
-def run_build_frequency(team_uuid, primary, secondary):
+def run_build_frequency(team_uuid, primary, secondary, start, end):
     team = Team.query.filter(Team.uuid == team_uuid).first()
     primary = team.roles.filter(Role.name == primary).first()
     secondary = team.roles.filter(Role.name == secondary).first()
@@ -17,5 +28,5 @@ def run_build_frequency(team_uuid, primary, secondary):
  
     return {
         "header": [" "] + [u.username for u in secondary_users],
-        "pairs": [frequencies_for_user(user, secondary_users) for user in primary_users],
+        "pairs": [frequencies_for_user(user, secondary_users, start, end) for user in primary_users],
     }
--- pairamid_api/pair_frequency/routes.py
+++ pairamid_api/pair_frequency/routes.py
@@ -6,6 +6,9 @@ blueprint = Blueprint("pair_frequency", __name__)
 
 @blueprint.route("/team/<team_uuid>/frequency", methods=["GET"])
 def fetch_frequency(team_uuid):
+    start = request.args.get("startDate")
+    end = request.args.get("endDate")
     primary = request.args.get("primary")
     secondary = request.args.get("secondary")
-    return jsonify(operations.run_build_frequency(team_uuid, primary, secondary))
+
+    return jsonify(operations.run_build_frequency(team_uuid, primary, secondary, start, end))
