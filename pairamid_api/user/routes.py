cd176a7660180da502f3522ca452c0a768126e03
Erik Olsen <erik.s.olsen@gmail.com>
2021-03-06 19:16:15 -0600

    Adds user auth functions

--- pairamid_api/user/routes.py
+++ pairamid_api/user/routes.py
@@ -37,8 +37,6 @@ def revive(team_uuid, id):
 @flask_praetorian.auth_required
 def user_show(id):
     user = flask_praetorian.current_user()
-    print('user.uuid: ', user.uuid)
-    print('id: ', id)
     if str(user.uuid) == id:
         return jsonify(operations.run_fetch(id))
     raise Exception('Please login to continue.')
@@ -46,3 +44,25 @@ def user_show(id):
 @blueprint.route("/users", methods=["POST"])
 def user_create():
     return jsonify(operations.run_sign_up(request.json))
+
+
+@blueprint.route("/login", methods=["POST"])
+def user_login():
+    req = request.get_json(force=True)
+    email = req.get("email", None)
+    password = req.get("password", None)
+    try:
+        user = guard.authenticate(email, password)
+        result = {
+            "access_token": guard.encode_jwt_token(user),
+            "uuid": user.uuid,
+        }
+        return jsonify(result)
+    except Exception as e:
+        raise e
+
+@blueprint.route("/refresh")
+def refresh():
+    old_token = guard.read_token_from_header()
+    new_token = guard.refresh_jwt_token(old_token)
+    return jsonify(access_token=new_token)
