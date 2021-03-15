6369239178fa8d90fdf888f0724ba3ad4a9a5806
Erik Olsen <erik.s.olsen@gmail.com>
2021-03-14 21:51:51 -0500

    Dont start users with essential virtues

--- pairamid_api/user/initial_feedback_groups.py
+++ pairamid_api/user/initial_feedback_groups.py
@@ -20,22 +20,5 @@ INITIAL_FEEDBACK_GROUPS = [
                 "description": "Examples and suggestions highlighting areas of improvement",
             }
         ]
-    },
-    {
-        "name": "Essential Virtues",
-        "tags": [
-            {
-                "name": "Humble",
-                "description": "does not think less of self; thinks of self less",
-            },
-            {
-                "name": "Hungry",
-                "description": "aggressively pursues goals",
-            },
-            {
-                "name": "Smart",
-                "description": "emotionally smart, that is, in interactions with others",
-            }
-        ]
-    },
+    }
 ]
