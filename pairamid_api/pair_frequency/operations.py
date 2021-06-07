34dc17905fb33bbbaa47ffa1066c4bc9dcbe04ca
Erik Olsen <erik.s.olsen@gmail.com>
2021-06-07 18:18:40 -0500

    Update test to include default date params

--- pairamid_api/pair_frequency/operations.py
+++ pairamid_api/pair_frequency/operations.py
@@ -1,4 +1,5 @@
-from datetime import datetime 
+from datetime import datetime
+import pendulum
 from pairamid_api.lib.date_helpers import start_of_day, end_of_day
 from pairamid_api.extensions import db
 from pairamid_api.models import User, PairingSession, Role, Team
@@ -7,9 +8,10 @@ from collections import Counter
 def parse_date(iso_date_string):
     return datetime.strptime(iso_date_string, '%Y-%m-%d')
 
-def frequencies_for_user(user, group, start, end):
-    start = start_of_day(parse_date(start))
-    end = end_of_day(parse_date(end))
+def frequencies_for_user(user, group, start=None, end=None):
+    today = pendulum.now()
+    start = start_of_day(parse_date(start or today.to_date_string()))
+    end = end_of_day(parse_date(end or today.add(days=1).to_date_string() ))
     sessions = (user.pairing_sessions
                     .filter(~PairingSession.info.in_(PairingSession.FILTERED))
                     .filter(PairingSession.created_at >= start)
@@ -19,7 +21,7 @@ def frequencies_for_user(user, group, start, end):
     counts[user.username] = len([p for p in sessions if len(p.users) == 1])
     return [user.username, *[counts.get(u.username, 0) for u in group]]
 
-def run_build_frequency(team_uuid, primary, secondary, start, end):
+def run_build_frequency(team_uuid, primary, secondary, start=None, end=None):
     team = Team.query.filter(Team.uuid == team_uuid).first()
     primary = team.roles.filter(Role.name == primary).first()
     secondary = team.roles.filter(Role.name == secondary).first()
